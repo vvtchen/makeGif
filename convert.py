f823e5422cb45e0a854cad2d63a2290c468b4490
Aries Chen <arieschen@Ariess-MacBook-Air-2.local>
2023-07-04 11:36:51 +0800

    add ffmpeg

--- convert.py
+++ convert.py
@@ -1,13 +1,13 @@
 import os
-os.environ["IMAGEIO_FFMPEG_EXE"] = "/Users/arieschen/Downloads/ffmpeg"
+os.environ["IMAGEIO_FFMPEG_EXE"] = "./ffmpeg"
 
 from moviepy.editor import *
 
 for fileName in os.listdir("source"):
     filePath = os.path.join("source", fileName)
     videoClip = VideoFileClip(filePath)
-    videoClip = videoClip.fx( vfx.speedx, 10)
-    videoClip = videoClip.fx( vfx.resize, height=900)
+    videoClip = videoClip.fx( vfx.speedx, 20)
+    videoClip = videoClip.fx( vfx.resize, height=500)
     videoClip.write_gif(fileName[0:len(fileName)-4]+".gif", fps=25, program="ffmpeg")
     
 
